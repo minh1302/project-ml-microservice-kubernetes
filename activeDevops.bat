da7431f17fc1ac848185d15c7fca3a8941d242c4
thoangminh <truonghoangminh1302@gmail.com>
2022-05-13 15:08:17 +0700

    update

---
+++ activeDevops.bat
@@ -0,0 +1,1 @@
+C:\Users\MinhTH6\.devops\Scripts\activate && cd /d "D:\OneDrive - FPT Software\Learning\Udacity_AWS_DevOps\Output\Project_4_Operationalize_a_Machine_Learning_Microservice_API\project-ml-microservice-kubernetes"
