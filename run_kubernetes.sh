b0aa18cf79dd08ed7b9bf38bc9aea758df06e838
minh1302 <truonghoangminh1302@gmail.com>
2022-05-14 04:39:23 +0000

    update

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=minh1302/udacity-project-3
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl get nodes
+kubectl create deploy udacity-project-3.0 --image=minh1302/udacity-project-3
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get deploy,rs,svc,pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/udacity-project-3.0 --address 0.0.0.0 8000:80
